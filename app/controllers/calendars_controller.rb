c01d68c3559b08f7f3ff56b83431daafb3c5ddf6
kensuke-iwata <kensukeiwata1313@gmail.com>
2021-02-17 13:05:04 +0900

    重複したコードの削除

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -18,10 +18,6 @@ class CalendarsController < ApplicationController
     params.require(:plan).permit(:date, :plan)
   end
 
-  def plan_paramas
-    params.require(:calendars).permit(:date, :plan)
-  end
-
   def get_week
 
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
@@ -47,7 +43,6 @@ class CalendarsController < ApplicationController
       end
 
       days = { month: (@todays_date + x).month, date: (@todays_date + x).day, plans: today_plans, wday: wdays[wday_num] }
-
       @week_days.push(days)
     end
   end
